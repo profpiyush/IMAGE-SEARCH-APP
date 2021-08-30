9e3b6f414acb6d231379abd53950b1b810787aa1
Piyush Awasthi <profpiyush1905@gmail.com>
2021-08-30 10:35:21 +0530

    added the navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react'
 import './style.css';
 import InputBox from './components/InputBox';
 import Gallery from './components/Gallery';
+import Navbar from './components/Navbar';
 
 function App() {
   const [input, setInput] = useState("");
   return (
     <div className="App">
-      <h1>Image Search App!!!</h1>
+      <Navbar />
       <InputBox input={input} setInput={setInput} />
       <Gallery query={input} />
     </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span>CLIXELS - The image search app</span>
+        </div>
+    )
+}
+
+export default Navbar
